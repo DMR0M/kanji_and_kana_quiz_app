a416d66858f813ecc9a7ad19b6fa75119b8dc6ac
Rommel Rudolf Dela Merced <rdelamerced@tspi.com.ph>
2024-04-16 11:35:51 +0800

    added SQL insert script for importing data to Kanji Definitions table

---
+++ scripts/Kanji_Definitions_Data.sql
@@ -0,0 +1,23 @@
+INSERT INTO kanji_quiz_app_kanjiquiz(kanji_character, hiragana_character, reading, meaning)
+VALUES
+    ('健康', 'けんこう', 'kenkou', 'health'),
+    ('管理', 'かんり', 'kanri', 'management'),
+    ('個人', 'こじん', 'kojin', 'individual'),
+    ('印刷', 'いんさつ', 'insatsu', 'printing'),
+    ('検索', 'けんさく', 'kensaku', 'search'),
+    ('削除', 'さくじょ', 'sakujo', 'delete'),
+    ('番号', 'ばんごう', 'bangou', 'number'),
+    ('住民', 'じゅうみん', 'juumin', 'citizen'),
+    ('選択', 'せんたく', 'sentaku', 'selection'),
+    ('更新', 'こうしん', 'koushin', 'update'),
+    ('編集', 'へんしゅう', 'henshuu', 'edit'),
+    ('検診', 'けんしん', 'kenshin', 'medical checkup'),
+    ('性別', 'せいべつ', 'seibetsu', 'gender'),
+    ('生年月日', 'せいねんがっぴ', 'seinengappi', 'date of birth'),
+    ('結果', 'けっか', 'kekka', 'result'),
+    ('年齢', 'ねんれい', 'nenrei', 'age'),
+    ('登録', 'とうろく', 'touroku', 'registration'),
+    ('取込', 'とりこみ', 'torikomi', 'capture'),
+    ('追加', 'ついか', 'tsuika', 'addition'),
+    ('処理', 'しょり', 'shori', 'process');
+    
