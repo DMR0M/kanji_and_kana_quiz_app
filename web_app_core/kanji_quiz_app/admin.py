430fc16a4a37ee2f94b7141a7a7769931376a4af
Rommel <rommeldm87@gmail.com>
2024-04-20 20:47:38 +0800

    registered definitions table to admin

--- web_app_core/kanji_quiz_app/admin.py
+++ web_app_core/kanji_quiz_app/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
-from .models import KanjiQuiz
+from .models import KanjiQuiz, HiraganaQuiz, KatakanaQuiz
 
 
-# Register your models here.
 admin.site.register(KanjiQuiz)
+admin.site.register(HiraganaQuiz)
+admin.site.register(KatakanaQuiz)
